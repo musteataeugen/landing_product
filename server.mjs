9a2487e9f073c12d40577715ba8ae3de468657a7
musteata88 <musteataeugen2022@gmail.com>
2024-05-28 17:24:15 +0300

    api+fetch DOM rendering

--- server.mjs
+++ server.mjs
@@ -10,49 +10,17 @@ const routes = {
   "/images/product.jpg": "images/product.jpg"
 }
 
-//HW1*: what if product data would be stored as an XML?
-
-//HW2:imagine that "tags": [ "books", "JS", "design", "learning", "education", "2018", "programming", "IT"],
-//    is added to your json file:
-//    -refactor the html template
-//    -refactor the server script using a loop so the tags would be rendered as an unordered html list
-
-// 2. create the server object
-
 const server = http.createServer((req, res) => {
-  // router
-  // 2 varianata
+  //page router  
   if (routes[req.url]) {
     fs.readFile(routes[req.url], (err, data) => {
-      //1. react only for the main page template
-      if (routes[req.url] == "index.html") {  
-
-        //2.load the product from json
-        fs.readFile("data/product.json", (err, dataJSON) => {
-          let productData = JSON.parse(dataJSON);
-
-          //3.replace the date in the html template
-          data = data.toString();
-          data = data.replace("{title}", productData.name);
-          data = data.replace("{image}", productData.image);
-          data = data.replace("{subtitle}", productData.subtitle);
-          data = data.replace("{description}", productData.description);
-          data = data.replace("{priceAmount}", productData["price"]["Amount"]);
-          data = data.replace("{priceCurrency}", productData["price"]["Currency"]);
-
-         data = data.replace("{tags}", productData.tags.map(tag => `<li>${tag}</li>`).join(""));                  
-        
-          res.write(data)
-          res.end()
-        })
-
-      } else {
-        res.write(data)
-        res.end()
-      }
-
+      res.write(data)
+      res.end()
     })
-  }  else {
+  } else if (req.url == "/api/product") {
+   
+  }
+   else  {
     res.write("Not found")
     res.end()
   }
